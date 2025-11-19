e66e9ae8ecfff2745a84977040f2cf729a846ac7
Dylan Scully <157177709+JohnProgramming08@users.noreply.github.com>
2025-11-19 18:13:17 +0000

    feat: add log in and sign up forms

---
+++ MxV/backend/forms/sign_up.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Length
+
+
+class LoginForm(FlaskForm):
+    email = EmailField(
+        "Email", render_kw={"placeholder": "Email"}, validators=[DataRequired()]
+    )
+    password = PasswordField(
+        "Password",
+        render_kw={"placeholder": "Password"},
+        validators=[DataRequired(), Length(min=5, max=25)],
+    )
+    log_in = SubmitField("Log in")
