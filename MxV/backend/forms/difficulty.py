785e2dc43d3cd1f27941940357862110995d31f1
Dylan Scully <157177709+JohnProgramming08@users.noreply.github.com>
2026-01-08 16:17:33 +0000

    feat: Add first few questions for testing

--- MxV/backend/forms/difficulty.py
+++ MxV/backend/forms/difficulty.py
@@ -4,6 +4,7 @@ from wtforms import BooleanField
 class DifficultyForm(FlaskForm):
 	class Meta:
 		csrf = False
+		
 	practice1 = BooleanField("Practice 1")
 	practice2 = BooleanField("Practice 2")
 	practice3 = BooleanField("Practice 3")
