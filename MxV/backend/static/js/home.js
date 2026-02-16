6544eef4c15f94a07f4eca1e4a26a852171cb365
Dylan Scully <157177709+JohnProgramming08@users.noreply.github.com>
2026-02-16 23:56:31 +0000

    feat: finish achievements system

--- MxV/backend/static/js/home.js
+++ MxV/backend/static/js/home.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const momentumData = window.appData.momentum_data;
+	const userData = window.appData.user_data;
+	const answerData = window.appData.answer_data;
 	const bars = Array.from(document.getElementsByClassName("bar"));
+	const achievementsTable = document.getElementById("achievements-table");
 
 	// Adjust the width of given momentum bar
 	function adjustBarWidth(bar, percent) {
@@ -18,4 +21,26 @@ document.addEventListener("DOMContentLoaded", () => {
 		const percent = momentumData[id];
 		adjustBarWidth(bar, percent);
 	});
+
+	// All of the achievements a user can get
+	// Name, condition, filepath
+	const achievements = [
+		["Getting Started", answerData.total >= 1, "getting_started.jpg"],
+		["Dedicated Student", answerData.total >= 100, "dedicated_student.jpg"],
+		["Seriously?", answerData.total >= 1000, "seriously.png"],
+		["Sigma", momentumData.total >= 67, "sigma.png"],
+		["Exam Prepped", momentumData.exam2 >= 100, "exam_prepped.png"]
+	];
+
+	// Display all of the users achievements
+	function displayAchievements() {
+		for (const achievement of achievements) {
+			if (achievement[1]) {
+				const temp = `<img class="achievement-img" src="/static/images/achievements/${achievement[2]}"/>`
+				achievementsTable.innerHTML += temp;			
+			}
+		}
+	}
+
+	displayAchievements();
 });
